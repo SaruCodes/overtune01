64abdbc3b714b7a3f39524617668480c78e24943
Sara O. <ortegasara204@gmail.com>
2025-03-06 20:24:40 +0100

    añadidas traducciones

--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -10,20 +10,20 @@
     <!-- Menú de Navegación -->
     <div class="flex-none">
         <ul class="menu menu-horizontal px-1 space-x-4 flex items-center h-full">
-            <li><a class="btn btn-outline btn-secondary" href="{{ route('home') }}">Inicio</a></li>
-            <li><a class="btn btn-outline btn-secondary" href="{{ route('novedades') }}">Novedades</a></li>
-            <li><a class="btn btn-outline btn-secondary" href="">Mejor puntuados</a></li>
-            <li><a class="btn btn-outline btn-secondary" href="">Artistas</a></li>
+            <li><a class="btn btn-outline btn-secondary" href="{{ route('home') }}">{{ __('Inicio') }}</a></li>
+            <li><a class="btn btn-outline btn-secondary" href="{{ route('novedades') }}">{{ __('Novedades') }}</a></li>
+            <li><a class="btn btn-outline btn-secondary" href="">{{ __('Mejor puntuados') }}</a></li>
+            <li><a class="btn btn-outline btn-secondary" href="">{{ __('Artistas') }}</a></li>
             <li class="flex items-center">
                 <x-layouts.lang />
             </li>
             <li class="flex items-center relative z-50">
                 @guest
                     <details class="relative flex items-center z-50">
-                        <summary class="btn btn-outline btn-primary">Acceso</summary>
+                        <summary class="btn btn-outline btn-primary">{{ __('Acceso') }}</summary>
                         <ul class="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-violet-900 text-white rounded-md shadow-lg p-2 z-50">
-                            <li><a class="block px-4 py-2 hover:bg-violet-800 rounded-md" href="{{ route('login') }}">{{__('Login')}}</a></li>
-                            <li><a class="block px-4 py-2 hover:bg-violet-800 rounded-md" href="{{ route('register') }}">{{__('Register')}}</a></li>
+                            <li><a class="block px-4 py-2 hover:bg-violet-800 rounded-md" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <li><a class="block px-4 py-2 hover:bg-violet-800 rounded-md" href="{{ route('register') }}">{{ __('Registro') }}</a></li>
                         </ul>
                     </details>
                 @endguest
@@ -31,7 +31,7 @@
                     <span class="text-white mx-2">{{ auth()->user()->name }}</span>
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
-                        <input class="btn btn-primary" type="submit" value="Logout">
+                        <input class="btn btn-primary" type="submit" value="{{ __('Cerrar sesión') }}">
                     </form>
                 @endauth
             </li>
